3502819422336b139ce400f231995aa402efa994
Sylvain <schambon@mail.ccsf.edu>
2013-05-01 18:05:13 -0700

    Republish sound class files

--- Match3/src/Audio/SoundEffect.java
+++ Match3/src/Audio/SoundEffect.java
@@ -1,139 +1,90 @@
 package Audio;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import org.newdawn.slick.Music;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
 
 /**
- * This enum contains all sound effects of Match3 game. It defines
- * also a static variable 'SoundEffect.volume' to enable a mute mode
- * for the game (read further).
- * 
- * INSTRUCTIONS:
- * 
- * 1/ To add a sound effect, simply add an item to the list
- *    in the following format: SOUNDNAME('fileName.wav') where
- *    SOUNDNAME is the name of the sound that will be called in the 
- *    program and 'fileName.wav' the sound file (wav format) that will
- *    be placed into the '/res' directory.
- * 
- * 2/ To play a sound, just call SoundEffect.SOUNDNAME.play()
- *    anywhere in the program.
- * 
- * 3/ For the mute mode, set the static variable SoundEffect.volume to OFF
- *    by doing SoundEffect.volume = Volume.OFF
- * 
- * 
- * I've based my code on this webpage:
- * http://www3.ntu.edu.sg/home/ehchua/programming/java/J8c_PlayingSound.html
- * @author sylvain
+ * Enumeration of all sound effect available.
+ * @author sylcha
  *
  */
 public enum SoundEffect {
-//-------------------------------------------------------------------------
-//                   VARIABLES
-//-------------------------------------------------------------------------
-	// Available sound effects
-	// Play one with 'SoundEffect.SOUNDNAME.play()'
+	// Add the files in the '/Content/SoundClips' directory
+	// than add the sound to the list following the syntax:
+	// EFFECT_NAME("sound_file_name")
 	SELECT("select.wav"),
 	SWAP("swap.wav");
 	
-	 // Each sound effect has its own clip, loaded with its own sound file.
-	 private Clip clip;
-	 
-	 // Nested enum to enable a volume switch ON/OFF button
-	 // NOTE: Is declared 'static' to be able to switch on/off
-	 //       all sounds at the same time.
-	 public static enum Volume {
-		 ON, 
-		 OFF;
-	 }
-	 
-	 // Volume is 'ON' by default
-	 public static Volume volume = Volume.ON;
-
-//-------------------------------------------------------------------------
-//              METHODS	 
-//-------------------------------------------------------------------------
-	 /**
-	  * Constructor: build each item on the enum with
-	  * its own sound file
-	  * @param soundFileName sound file name
-	  */
+	private Sound effect; // clip that handles the sound effect
+	private static boolean mute = false; // flag for mute status
+	
+	public static float volume = 1.0f; // volume level 
+	private static float oldVolume; // to retrieve last vol level after mute
+	
+	/**
+	 * Constructor.
+	 * @param soundFileName name of the sound file (String)
+	 */
 	SoundEffect(String soundFileName) {
+		char sep = File.separatorChar; // default name-separator character 
+		String path =  sep
+				     + "SoundClips" + sep
+				     + soundFileName;
+		URL url = getClass().getResource(path);
+		
 		try {
-			// **** Locate the file ****
-			//
-			// 1 - build the relative path for the resource
-			// 2 - use an URL object to build the absolute path
-			//     for the resource
-			// 3 - get the file from the URL object
-			//
-			// NOTE: be sure that the classpath contains
-			//       the '/Content' folder
-			char sep = File.separatorChar; // default name-separator character 
-			String path =  sep
-					     + "SoundClips" + sep
-					     + soundFileName;
-			URL url = getClass().getResource(path);
-			File soundFile = new File(url.getPath());
+			effect = new Sound(url);
 			
-			// Set up an audio input
-			AudioInputStream audioInputStream 
-				= AudioSystem.getAudioInputStream(soundFile);
-			
-			// Get a clip resource.
-			clip = AudioSystem.getClip();
-	         
-			// Open audio clip and load samples from the audio input stream.
-			clip.open(audioInputStream);
-		} 
-		catch (UnsupportedAudioFileException e) {
-			e.printStackTrace();
-		}
-		catch (IOException e){
-			e.printStackTrace();
-		}
-		catch (LineUnavailableException e) {
+		} catch (SlickException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	/**
-	 * Plays a sound. The method stops the clip if running, 
-	 * rewinds to the beginning of the clip and plays it again
-	 * to avoid overlap.
+	 * Play the sound at the general volume level.
 	 */
 	public void play() {
-		if (volume == Volume.ON) {
-			if (clip.isRunning())
-				clip.stop();          // 1. stop the clip if running
-			clip.setFramePosition(0); // 2. rewind
-			clip.start();             // 3. start a new time
+		effect.play(0.5f, volume);
+	}
+	
+	/**
+	 * Increase the volume of 10%.
+	 * Set the static variable 'volume' to a new value less than 1.0f.
+	 */
+	public static void volumeUp() {
+		// increase of 10%
+		if (!mute)
+			volume += 0.1; 
+		// control the maximum value
+		if (volume > 1.0f)
+			volume = 1.0f;
+	}
+	
+	/**
+	 * Decrease the volume of 10%
+	 * Set the static variable 'volume' to a new value more than 0.0f.
+	 */
+	public static void volumeDown() {
+		// decrease of 10%
+		volume -= 0.1;
+		// control the minimum value
+		if (volume < 0.0f)
+			volume = 0.0f;
+	}
+	
+	public static void switchSound() {
+		if (!mute) {
+			oldVolume = volume;
+			volume = 0.0f;
+			mute = true;
+		}
+		else {
+			volume = oldVolume;
+			mute = false;
 		}
 	}
-	
-	/**
-	 * Stop a sound which is playing.
-	 */
-	public void stop() {
-		if (volume == Volume.ON)
-			if (clip.isRunning())
-				clip.stop();
-	}
-	
-	/**
-	 * Pre-load all the sounds files to avoid pausing while
-	 * loeading the sound file for the first time.
-	 */
-	public static void init() {
-		values(); // call the constructor for all the element in the enum
-	}
-	
 }
--- Match3/src/Audio/SoundTrack.java
+++ Match3/src/Audio/SoundTrack.java
@@ -1,127 +1,138 @@
 package Audio;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import org.newdawn.slick.Music;
+import org.newdawn.slick.SlickException;
 
-import Audio.SoundEffect.Volume;
-
-
+/**
+ * This enumeration handle the music tracks for background music.
+ * It can handle one track at a time!
+ * @author sylcha
+ *
+ */
 public enum SoundTrack {
-//-------------------------------------------------------------------------
-//  VARIABLES
-//-------------------------------------------------------------------------
-	// Available sound tracks
-	// Play one with 'SoundTrack.TRACK_NAME.play()'
-	TRACK_ONE("track1.wav");
+	// Add the files in the '/Content/SoundClips' directory
+	// than add the sound to the list following the syntax:
+	// TRACK_NAME("sound_file_name")
+TRACK_ONE("track1.wav");
 	
-	private Clip track; // SoundTrack object has its own clip
-	private int currentPosition; // current position in the stream
-	
-	// Nested enum to enable a volume switch ON/OFF button
-	// NOTE: Is declared 'static' to be able to switch on/off
-	//       all sounds at the same time.
-	public static enum Volume {
-		ON, 
-		OFF;
-	}
-	 
-	// Volume is 'ON' by default
-	public static Volume volume = Volume.ON;
+	Music track; // clip that handle the track
+	public static float volume = 1.0f; // volume level
+	private static boolean mute = false; // flag for mute status
+	public static float oldVolume; // to retrieve last vol level after mute
 
-//-------------------------------------------------------------------------
-//  METHODS	 
-//-------------------------------------------------------------------------
 	/**
-	 * Constructor
-	 * @param soundTrackName name of the sound track file
+	 * Constructor.
+	 * @param soundFileName name of the sound file (String)
 	 */
-	SoundTrack(String soundTrackName) {
+	SoundTrack(String soundFileName) {
+		char sep = File.separatorChar; // default name-separator character 
+		String path =  sep
+				     + "SoundClips" + sep
+				     + soundFileName;
+		URL url = getClass().getResource(path);
 		
 		try {
-			// **** Locate the file ****
-			//
-			// 1 - build the relative path for the resource
-			// 2 - use an URL object to build the absolute path
-			//     for the resource
-			// 3 - get the file from the URL object
-			//
-			// NOTE: be sure that the classpath contains
-			//       the '/Content' folder
-			char sep = File.separatorChar; // default name-separator character 
-			String path =  sep
-					     + "SoundClips" + sep
-					     + soundTrackName;
-			URL url = getClass().getResource(path);
-			File soundFile = new File(url.getPath());
-			
-			
-			// Set up an audio input
-			AudioInputStream audioInputStream 
-				= AudioSystem.getAudioInputStream(soundFile);
-			
-			// Get a clip resource.
-			track = AudioSystem.getClip();
-	         
-			// Open audio clip and load samples from the audio input stream.
-			track.open(audioInputStream);
-			
-			// Initialize the currentPosition to 0
-			currentPosition = 0;
-			
-			
-
-		} 
-		catch (UnsupportedAudioFileException e) {
-			e.printStackTrace();
-		}
-		catch (IOException e){
-			e.printStackTrace();
-		}
-		catch (LineUnavailableException e) {
+			track = new Music(url);
+		} catch (SlickException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	/**
-	 * Play the track indefinitely from the current position
+	 * Play the track
 	 */
 	public void play() {
-		if (volume == Volume.ON) {
-			if (track.isRunning())
-				track.stop();
-			track.setFramePosition(currentPosition);
-			track.loop(Clip.LOOP_CONTINUOUSLY); //play indefinitely
+		if(!mute) {
+			if (!track.playing()) {
+				if (track.paused())
+					track.resume();
+				else
+					track.loop();
+			}
 		}
 	}
 	
 	/**
-	 * Stop the track playing and keep track of the current position
-	 * for future playing.
+	 * Pause the track
 	 */
 	public void pause() {
-		if (volume == Volume.ON)
-			if (track.isRunning()) {
-				currentPosition = track.getFramePosition();
-				track.stop();
-			}
+		if(!mute) {
+			if (track.playing())
+				track.pause();
+		}
 	}
 	
 	/**
-	 * Stop the track playing and set the current position to 0
+	 * Stop the track
 	 */
 	public void stop() {
-		if(volume == Volume.ON)
-			if (track.isRunning()) {
-				track.stop();
-				currentPosition = 0; // set current position to the beginning
+		if (!mute) {
+			if (track.playing()) {
+				track.pause();
+				track.setPosition(0.0f);
 			}
+		}
 	}
-
+	
+	/**
+	 * Set the volume of the background music (not the general volume).
+	 * @param vol
+	 */
+	public void setVolume(float vol) {
+		volume = vol;
+		track.setVolume(volume);
+	}
+	
+	/**
+	 * Increase the volume of 10%.
+	 * Set the static variable 'volume' to a new value less than 1.0f
+	 * and set the volume of the current using this value.
+	 */
+	public void volumeUp() {
+		if (!mute) {
+			// increase of 10%
+			volume += 0.1; 
+			// control the maximum value
+			if (volume > 1.0f)
+				volume = 1.0f;
+			// set the volume for the current clip
+			setVolume(volume);
+		}
+	}
+	
+	/**
+	 * Decrease the volume of 10%
+	 * Set the static variable 'volume' to a new value more than 0.0f
+	 * and set the volume of the current clip using this value.
+	 */
+	public void volumeDown() {
+		if (!mute) {
+			// decrease of 10%
+			volume -= 0.1;
+			// control the minimum value
+			if (volume < 0.0f)
+				volume = 0.0f;
+			// set the volume for the current clip
+			setVolume(volume);
+		}
+	}
+	
+	/**
+	 * Turn the sound off if on and vice-versa.
+	 */
+	public void switchSound() {
+		if (!mute) {
+			oldVolume = volume;
+			volume = 0.0f;
+			mute = true;
+		}
+		else {
+			volume = oldVolume;
+			mute = false;
+		}
+		this.setVolume(volume);
+	}
 }
